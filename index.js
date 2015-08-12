a2cfa99c40c11680156fef5fdb069abe3f3d6071
Andreas Hocevar <andreas.hocevar@gmail.com>
2015-08-12 12:39:01 +0200

    Add support for multiple stage sets

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 (function() {
-  var stages = [8, 9, 10, 11, 12];
+  var stageOptions = [[8, 9, 10, 11, 12], [8, 9.5, 11, 12.5, 14]];
   var dist = 1200;
   var i = -1;
 
+  var stages = stageOptions[0];
+  for (var s = 0, ss = stageOptions.length; s < ss; ++s) {
+    $('#stages').append('<li><a href="#">' + stageOptions[s].join(' &ndash; ') + ' km/h</a></li>');
+  }
+  $('#dropdownMenu1').html($('#stages li a').html() + ' <span class="caret"></span>');
+  $('#stages li a').click(function(){
+    var selText = $(this).text();
+    $('#dropdownMenu1').html(selText + ' <span class="caret"></span>');
+    stages = stageOptions[$('#stages li a').index($(this))];
+    firstStage();
+  });
+
+  var distRemaining = dist;
+  function firstStage() {
+    i = -1;
+    nextStage();
+  }
   function nextStage() {
     ++i;
     if (i >= stages.length) {
@@ -10,11 +27,11 @@
       $('#start').addClass('btn-success').removeClass('btn-danger').removeClass('btn-success');
       $('#start').html('<span class="glyphicon glyphicon-refresh"></span> Reload');
     } else {
-      dist = 1200;
+      distRemaining = dist;
       $('#start').addClass('btn-success').removeClass('btn-danger').removeClass('btn-default');
       $('#start').html('<span class="glyphicon glyphicon-play"></span> Start');
       $('#stage').html('Stage ' + (i + 1) + ' &mdash; ' + stages[i] + ' km/h');
-      $('#dist').html(dist + ' m');
+      $('#dist').html(distRemaining + ' m');
     }
   }
 
@@ -31,10 +48,10 @@
       $('#start').addClass('btn-danger').removeClass('btn-success').removeClass('btn-default');
       $('#start').html('<span class="blink glyphicon glyphicon-stop"></span> Stop');
       interval = window.setInterval(function() {
-        dist -= 50;
-        $('#dist').html(dist + ' m');
+        distRemaining -= 50;
+        $('#dist').html(distRemaining + ' m');
         sound.play();
-        if (dist <= 0) {
+        if (distRemaining <= 0) {
           window.clearInterval(interval);
           nextStage();
         }
@@ -43,8 +60,7 @@
       window.clearInterval(interval);
       nextStage();
     } else {
-      i = -1;
-      nextStage();
+      firstStage();
     }
   });
 })();
